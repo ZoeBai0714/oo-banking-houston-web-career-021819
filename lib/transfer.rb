2d844d2938f7e3380d00a466b4cf068b16ff09fe
Zoe Bai <s.xue.bai@gmail.com>
2019-02-22 20:32:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,21 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending"
+  end
+  
+  def valid?
+    if @sender.valid? && @receiver.valid?
+      return true
+    end
+  end
+  
+  def execute_transaction
+    BankAccount.new(@sender).balance = @amount
+    BankAccount.new(@receiver).balance += @amount
+  end
 end
