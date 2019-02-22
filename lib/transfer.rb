325fb51673453edd88f22dbea0037a8bc63a2d1b
Zoe Bai <s.xue.bai@gmail.com>
2019-02-22 20:33:28 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -15,7 +15,7 @@ class Transfer
   end
   
   def execute_transaction
-    BankAccount.new(sender).balance = @amount
-    BankAccount.new(receiver).balance += @amount
+    BankAccount.new(sender).balance = amount
+    BankAccount.new(receiver).balance += amount
   end
 end
